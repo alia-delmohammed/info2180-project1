b82fac722d1e8f38a425c0474762682cb26a66e0
alia-delmohammed <114101310+alia-delmohammed@users.noreply.github.com>
2022-10-30 12:49:44 -0500

    Edited files

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 /* Add your JavaScript to this file */
 
 window.onload = function(){
-    var emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    var mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
     var email = document.getElementById("email");
-    var emailmsg = document.getElementsByClassName("message")[0];
-    var submit = document.getElementsByTagName("button")[0];
-    submit.addEventListener("click", (e)=>{
+    var emsg = document.getElementsByClassName("message")[0];
+    var submitbtn = document.getElementsByTagName("button")[0];
+    submitbtn.addEventListener("click", (e)=>{
         e.preventDefault();
 
-    if (email.value =="" || !(email.value.match(emailFormat))){
-        emailmsg.innerHTML = "Please enter a valid email address.";
+    if (email.value =="" || !(email.value.match(mailFormat))){
+        emsg.innerHTML = "Please enter a valid email address.";
         email.value ="";
     }else{
-        emailmsg.innerHTML = "Thank you! Your email address " + email.value + " has been added to our mailing list";
+        emsg.innerHTML = "Thank you! Your email address " + email.value + " has been added to our mailing list";
     }
 }
 )}
